620395ebd142d72f6f8bd66af0375a0204c56144
Awais Zaka <az@intsoftdev.com>
2022-11-26 17:48:14 +0000

    update snapshots for stations and services

--- convention-plugins/src/main/kotlin/convention.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/convention.publication.gradle.kts
@@ -42,7 +42,7 @@ val javadocJar by tasks.registering(Jar::class) {
 fun getExtraString(name: String) = ext[name]?.toString()
 
 group = "com.intsoftdev"
-version = "0.0.1"
+version = "0.0.2-SNAPSHOT"
 
 publishing {
     // Configure maven central repository
