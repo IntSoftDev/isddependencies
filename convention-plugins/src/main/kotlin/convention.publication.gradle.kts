b57bcd744627ca2d15c5912d0ccbfc1f4dfa17ae
Awais Zaka <az@intsoftdev.com>
2022-11-27 04:57:38 +0000

    update to 0.0.2

--- convention-plugins/src/main/kotlin/convention.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/convention.publication.gradle.kts
@@ -42,7 +42,7 @@ val javadocJar by tasks.registering(Jar::class) {
 fun getExtraString(name: String) = ext[name]?.toString()
 
 group = "com.intsoftdev"
-version = "0.0.2-SNAPSHOT"
+version = "0.0.2"
 
 publishing {
     // Configure maven central repository
