243fdf34d19e98e3ef148b148cd4edfbdb1e237b
Awais Zaka <az@intsoftdev.com>
2023-07-17 02:49:34 +0100

    update sdk versions

--- convention-plugins/src/main/kotlin/convention.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/convention.publication.gradle.kts
@@ -42,7 +42,7 @@ val javadocJar by tasks.registering(Jar::class) {
 fun getExtraString(name: String) = ext[name]?.toString()
 
 group = "com.intsoftdev"
-version = "0.0.51"
+version = "0.0.52"
 
 publishing {
     // Configure maven central repository
