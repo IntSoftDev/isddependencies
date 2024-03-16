bca4df3da0b36953654b6c8dd55904eeb1a89db4
Awais Zaka <az@intsoftdev.com>
2024-03-16 16:57:17 +0000

    update libs 0.0.6 snapshot

--- convention-plugins/src/main/kotlin/convention.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/convention.publication.gradle.kts
@@ -42,7 +42,7 @@ val javadocJar by tasks.registering(Jar::class) {
 fun getExtraString(name: String) = ext[name]?.toString()
 
 group = "com.intsoftdev"
-version = "0.0.53-SNAPSHOT"
+version = "0.0.6-SNAPSHOT"
 
 publishing {
     // Configure maven central repository
