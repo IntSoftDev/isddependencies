a290777e4326d39e4051d181fa1950cd1977c57d
Awais Zaka <az@intsoftdev.com>
2024-05-08 10:22:02 +0100

    update Napier and Turbine. Update version.

--- convention-plugins/src/main/kotlin/convention.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/convention.publication.gradle.kts
@@ -42,7 +42,7 @@ val javadocJar by tasks.registering(Jar::class) {
 fun getExtraString(name: String) = ext[name]?.toString()
 
 group = "com.intsoftdev"
-version = "0.0.61"
+version = "1.0.0-ALPHA-3"
 
 publishing {
     // Configure maven central repository
