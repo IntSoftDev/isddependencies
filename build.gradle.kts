fe2ff0109c9ec5b0e37e0d9e78a3cded219c3dab
Awais Zaka <az@intsoftdev.com>
2025-06-07 18:26:57 +0100

    update publishing mechanism. Update dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
+import com.vanniktech.maven.publish.SonatypeHost
 
+// https://vanniktech.github.io/gradle-maven-publish-plugin/central/
 plugins {
     `version-catalog`
-    `maven-publish`
-    id("convention.publication")
+    id("com.vanniktech.maven.publish") version "0.32.0"
 }
+
+group = "com.intsoftdev"
+version = "1.0.0-ALPHA-18-SNAPSHOT"
 
 catalog {
     versionCatalog {
@@ -11,11 +15,36 @@ catalog {
     }
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            artifactId = "isdversioncatalog"
-            from(components["versionCatalog"])
+mavenPublishing {
+    coordinates(
+        groupId = group.toString(),
+        artifactId = "isddependencies",
+        version = version.toString()
+    )
+
+    pom {
+        name.set("ISD Dependencies")
+        description.set("Gradle version catalog dependencies")
+        url.set("https://github.com/IntSoftDev/isddependencies")
+
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+        developers {
+            developer {
+                id.set("azaka01")
+                name.set("A Zaka")
+                email.set("az@intsoftdev.com")
+            }
+        }
+        scm {
+            url.set("https://github.com/IntSoftDev/isddependencies")
         }
     }
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
 }
